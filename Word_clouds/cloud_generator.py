c36082435b8bceeefa259580e8bb69da700df3d0
Hadi Mir <hmir20@student.oulu.fi>
2022-08-14 12:36:07 +0300

    Doc strings added in word clouds

--- Word_clouds/cloud_generator.py
+++ Word_clouds/cloud_generator.py
@@ -3,11 +3,18 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
 class WordCloudGenerator():
+    """This is the class implementation to generate word clouds
+    """
     def __init__(self, data_frame):
         self.data_frame = data_frame 
         self.dirName = ""
 
     def createOutputDir(self, dirName):
+        """This function creates output directory for file storage
+
+        Args:
+            dirName (str): Name of the directory you want to create
+        """
         self.dirName = dirName
         does_folder_exist = os.path.exists(f'../Output/{dirName}')
         if (does_folder_exist):
@@ -17,6 +24,13 @@ class WordCloudGenerator():
         print('Folder created for output storage')  
 
     def make_word_clouds(self, column, max_words=50, scale=2):
+        """This function generates the word cloud
+
+        Args:
+            column (str): Column in the data frame for which word cloud need to be generated
+            max_words (int, optional): Maximum number of words in the cloud. Defaults to 50.
+            scale (int, optional): The scale of the cloud. Defaults to 2.
+        """
         strings  = ' '.join(self.data_frame[column])
         wordcloud = WordCloud(scale=2, max_words=50, background_color="white").generate(strings)
         plt.figure(figsize = (20, 20))
@@ -27,6 +41,8 @@ class WordCloudGenerator():
 
 
     def date_based_comparasion_cloud(self):
+        """This function is used to generate the date based comparison cloud for titles
+        """
         mid_date = self.data_frame['Date'].astype('datetime64[ns]').quantile(0.5, interpolation="midpoint")
         new_titles = self.data_frame[(self.data_frame['Date']).astype('datetime64[ns]') >= mid_date]
         old_titles = self.data_frame[(self.data_frame['Date']).astype('datetime64[ns]') <  mid_date] 
